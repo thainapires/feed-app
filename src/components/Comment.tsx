6afab3616523a8065d69459b3ccc1b453f14bd0d
Thainá Pires <thainaspiress@gmail.com>
2023-02-08 23:27:07 -0300

    migrating from js to ts

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -3,7 +3,12 @@ import { ThumbsUp, Trash } from 'phosphor-react';
 import { Avatar } from './Avatar';
 import { useState } from 'react';
 
-export function Comment({content, onDeleteComment}){
+interface CommentProps {
+    content: string;
+    onDeleteComment: (comment: string) => void;
+}
+
+export function Comment({content, onDeleteComment}: CommentProps){
 
     const [likeCount, setLikeCount] = useState(0);
 
@@ -19,7 +24,12 @@ export function Comment({content, onDeleteComment}){
 
     return (
         <div className={styles.comment}>
-            <Avatar hasBorder={false} src="https://github.com/thainapires.png" alt=""/>
+            <Avatar 
+                hasBorder={false} 
+                src="https://github.com/thainapires.png" 
+                alt=""
+                //onClick={() => alert('teste')}
+            />
             <div className={styles.commentBox}>
                 <div className={styles.commentContent}>
                     <header>
